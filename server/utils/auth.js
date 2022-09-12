20e57e50d1ef914eb4aa2602e982a27d5943dbb8
xndroli <iamsimplyclean@knights.ucf.edu>
2022-09-12 18:37:43 -0400

    created queries.js and mutations.js logic

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
 	// function for our authenticated routes
-	authMiddleware: function (req) {
+	authMiddleware: function ({ req }) {
 		// allows token to be sent via  req.body, req.query or headers
 		let token = req.body.token || req.query.token || req.headers.authorization;
 
@@ -25,6 +25,7 @@ module.exports = {
 			req.user = data;
 		} catch {
 			console.log('Invalid token');
+			return res.status(400).json({ message: 'invalid token!' });
 		}
 
 		return req;
