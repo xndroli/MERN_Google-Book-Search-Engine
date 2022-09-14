0dcbcadfc2918ac53d6daf602d44a91ed4f2be7b
xndroli <iamsimplyclean@knights.ucf.edu>
2022-09-13 22:18:45 -0400

    updated ApolloClient and ApolloProvider import logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, ApolloProvider } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
