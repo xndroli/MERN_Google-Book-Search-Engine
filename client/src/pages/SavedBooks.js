8e13e5261a3e92dcc11bcc81f6932436c176c491
xndroli <iamsimplyclean@knights.ucf.edu>
2022-09-13 22:39:00 -0400

    changed savedBooks.js delete book import

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -9,13 +9,13 @@ import {
 
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
+import { DELETE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
 	const [loading, data] = useQuery(GET_ME);
-	const [deleteBook] = useMutation(REMOVE_BOOK);
+	const [deleteBook] = useMutation(DELETE_BOOK);
 	const userData = data?.me || {};
 
 	if (!userData?.username) {
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,4 @@
-import { gql } from 'graphql-tag';
+import { gql } from '@apollo/react-hooks';
 
 export const GET_ME = gql`
 	{
