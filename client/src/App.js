4acd09589772183965d4b099e0802c7d4b8a7863
xndroli <iamsimplyclean@knights.ucf.edu>
2022-09-13 22:05:38 -0400

    deploy to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from '@apollo/client';
+import { ApolloClient, ApolloProvider } from '@apollo/react-hooks';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
